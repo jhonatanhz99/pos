14d0a2dbc9acff1daa8ffc6c1c14c8e20c26c70a
Jhonatanhz99 <jhonatansolarte1125408371@gmail.com>
2026-05-29 22:04:52 -0500

    2

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login page title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Iniciar sesión/i);
   expect(linkElement).toBeInTheDocument();
 });
+
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,28 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock localStorage
+const localStorageMock = (function () {
+  let store = {};
+  return {
+    getItem: jest.fn((key) => store[key] || null),
+    setItem: jest.fn((key, value) => {
+      store[key] = value.toString();
+    }),
+    removeItem: jest.fn((key) => {
+      delete store[key];
+    }),
+    clear: jest.fn(() => {
+      store = {};
+    }),
+  };
+})();
+
+Object.defineProperty(global, 'localStorage', {
+  value: localStorageMock,
+  writable: true,
+});
+
+// Mock HTMLCanvasElement.prototype.getContext
+HTMLCanvasElement.prototype.getContext = jest.fn();
